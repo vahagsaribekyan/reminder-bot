f725f2c47446cb5a371c0d5669677b2c72f9c1f3
vahag <vahagn.saribekyan@matelso.com>
2025-03-10 14:22:31 +0400

    Fix db ocnfig

--- config/database.js
+++ config/database.js
@@ -6,9 +6,9 @@ const password = process.env.DATABASE_PASSWORD || 'postgres';
 const host = process.env.DATABASE_HOST || 'localhost';
 const dialect = process.env.DATABASE_DIALECT || 'postgres';
 
-const sequelize = new Sequelize('reminder_bot', 'postgres', 'postgres', {
-  host: 'localhost',
-  dialect: 'postgres'
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: dialect
 });
 
 module.exports = sequelize;
