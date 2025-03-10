5344666ca7b33fc6284e4eae310074d602844ee1
vahag <vahagn.saribekyan@matelso.com>
2025-03-10 15:08:00 +0400

    fix port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const sequelize = require('./config/database');
 const webhookRouter = require('./routes/webhook');
