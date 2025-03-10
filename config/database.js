5e2ea9accf783329b10144127d82523291a2ea10
vahag <vahagn.saribekyan@matelso.com>
2025-03-10 14:15:10 +0400

    Add bot setup

--- config/database.js
+++ config/database.js
@@ -1,5 +1,11 @@
 const { Sequelize } = require('sequelize');
 
+const database = process.env.DATABASE || 'reminder_bot';
+const username = process.env.DATABASE_USER || 'postgres';
+const password = process.env.DATABASE_PASSWORD || 'postgres';
+const host = process.env.DATABASE_HOST || 'localhost';
+const dialect = process.env.DATABASE_DIALECT || 'postgres';
+
 const sequelize = new Sequelize('reminder_bot', 'postgres', 'postgres', {
   host: 'localhost',
   dialect: 'postgres'
