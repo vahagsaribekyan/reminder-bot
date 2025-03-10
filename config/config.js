754a01a197beac1110c78dcffe847dd2bd92d47d
vahag <vahagn.saribekyan@matelso.com>
2025-03-10 13:17:02 +0400

    Add bot setup

--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  YOAI_API_KEY: '01957c6f-4a38-72be-8046-05b13214ee6f:bada8a53f765902b0cc83dd13b18bcdda4ddc154d2826e813d7c163baff368c0',
   BASE_URL: 'https://yoai.yophone.com/api/pub',
   POLLING_INTERVAL: 10000, // 10 seconds
-  OPENAI_API_KEY: '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'
 };
